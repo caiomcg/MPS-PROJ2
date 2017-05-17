64a054db43b34f0a1d9f34c3db17c4c410ad4f3f
Marcelo Aguiar Rodrigues <cabelo@gmail.com>
2017-05-17 19:09:46 -0300

    Playlist added.

--- src/business/model/Playlist.java
+++ src/business/model/Playlist.java
@@ -1,7 +1,25 @@
 package business.model;
 
+import java.util.Map;
+
 /**
  * Created by Marcelo on 17/05/2017.
  */
 public class Playlist {
+
+    private String name;
+    private Map<String, Artist> artistMap;
+
+    public Playlist(String name, Map<String, Artist> artistMap) {
+        this.name = name;
+        this.artistMap = artistMap;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, Artist> getArtistMap() {
+        return artistMap;
+    }
 }
